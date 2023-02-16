be5c7cde36b7914b11d384ffb55d4949d22688ad
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-16 16:01:47 -0400

    Update fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -14,10 +14,12 @@ class Solver
   end
 
   def fizzbuzz(number)
-    if number % 3 == 0
-      'fizz'
+    if number % 3 == 0 && number % 5 == 0
+      'fizzbuzz'
     elsif number % 5 == 0
       'buzz'
+    elsif number % 3 == 0
+      'fizz'
     else
       number.to_s
     end
