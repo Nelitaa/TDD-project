8df029863fed229788f17e81c4af71f85536000a
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-16 13:40:04 -0400

    Add test for factorial returns 1 for 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,10 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
+  describe '#factorial' do
+    it 'returns 1 for 0' do
+      solver = Solver.new
+      expect(Solver.factorial(0)).to eq(1)
+    end
+  end
 end
